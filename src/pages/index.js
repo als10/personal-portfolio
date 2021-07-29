73f16862cbd8e5be316016967840c61e064c2c9e
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-07-29 22:03:23 +0400

    add landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import { Layout } from "../components/layout"
-import { NavBar } from "../components/navbar"
+import { NavBar } from "../components/NavBar"
+import { Landing } from "../components/Landing"
 
 export default () => (
   <Layout>
     <NavBar />
+    <Landing />
   </Layout>
 )
 
