d0abb9137abc7402ab1b6731b2bfd745d59fd58f
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-07-30 19:25:28 +0400

    added experience section

---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Title } from "./Title"
+
+export const Experience = () => (
+  <section class="h-screen flex flex-row items-center">
+    <div>
+      <Title>Where I've worked</Title>
+      <div>Hello, this is text</div>
+    </div>
+  </section>
+)
