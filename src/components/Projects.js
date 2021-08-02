73772ebdfc7e645e2d75661ce2edf9af56501a59
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-08-02 14:26:48 +0400

    fix project card's responsiveness

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const projects = [
   },
   {
     title: 'Climb',
-    languages: ['C#', 'Unity'],
+    stack: ['C#', 'Unity'],
     date: 'May to June 2020',
     description: 'An Android game with simple controls, in which a player has to climb up a building without clicking in the wrong direction. Self-taught Unity and C# for its development and self-designed all in-game graphics.',
     link: '',
@@ -21,7 +21,7 @@ const projects = [
   },
   {
     title: 'Cricket Statistics Calculator',
-    languages: ['Java', 'SQLite', 'Android Studio'],
+    stack: ['Java', 'SQLite', 'Android Studio'],
     date: 'December 2018 to February 2019',
     description: 'An Android app that helps cricketers track the stats from all their matches and stores it in a database.',
     link: '',
@@ -29,7 +29,7 @@ const projects = [
   },
   {
     title: 'Coffee Farm Tools',
-    languages: ['Java', 'Android Studio'],
+    stack: ['Java', 'Android Studio'],
     date: 'July to September 2018',
     description: 'An Android app that helps coffee farmers by providing ready-made calculators for their various needs.',
     link: '',
@@ -37,7 +37,7 @@ const projects = [
   },
   {
     title: 'Tic-Tac-Toe AI',
-    languages: ['Python'],
+    stack: ['Python'],
     date: 'January 2021',
     description: 'An AI that uses the minimax algorithm to always win or draw a game of tic-tac-toe.',
     link: '',
@@ -45,7 +45,7 @@ const projects = [
   },
   {
     title: 'Online Course Management System',
-    languages: ['Python', 'MySQL', 'PyQt'],
+    stack: ['Python', 'MySQL', 'PyQt'],
     date: 'July 2020',
     description: 'A Python program that stores details of online courses from different websites in one place, and makes finding relevant online course easy with sort and search functionality.',
     link: '',
@@ -53,51 +53,53 @@ const projects = [
   },
 ]
 
-const Button = ({ text, url }) => (
-  <Link
-    class="p-4 rounded-lg bg-black text-white hover:bg-white hover:text-black hover:border-black"
-    to={url}
-  >
-    {text}
-  </Link>
-)
-
-const Technology = ({ item }) => (
-  <div class="bg-gray-200 p-2 rounded-md">
-    {item}
+const HighlightedText = ({ style, children }) => (
+  <div class={`text-sm sm:text-base p-2 m-1 rounded-lg ${style ?? ""}`}>
+    {children}
   </div>
 )
 
+const Button = ({ text, url }) => (
+  <HighlightedText style="xl:p-4 bg-black text-white hover:bg-white hover:text-black hover:border-black">
+    <Link to={url}>{text}</Link>
+  </HighlightedText>
+)
+
+const Technology = ({ item }) => (
+  <HighlightedText style="bg-gray-200">
+    {item}
+  </HighlightedText>
+)
+
 const Project = ({ project }) => (
-  <div class="w-full flex flex-col xl:flex-row space-x-8 space-y-8 bg-gray-300 m-8 p-8 md:p-16 rounded-xl items-center w-2/3">
+  <article class="flex flex-col xl:flex-row space-x-8 space-y-8 bg-gray-300 my-4 p-4 sm:p-8 md:p-16 rounded-xl items-center">
     <StaticImage
-      class="w-2/3 rounded-lg"
+      class="hidden sm:flex w-4/5 xl:w-3/5 rounded-lg"
       alt="project demo"
       src="../images/placeholder_image.png"
     />
-    <div class="flex flex-col space-y-4">
+    <div class="flex flex-col space-y-4 w-full xl:w-2/5">
       <div class="space-y-2">
         <h5>{project.title}</h5>
-        <p>{project.description}</p>
+        <p class="wrap">{project.description}</p>
       </div>
-      <div class="flex space-x-2">
+      <div class="flex flex-wrap">
         {project.stack.map(e => <Technology item={e} />)}
       </div>
-      <div class="flex space-x-2">
+      <div class="flex flex-wrap">
         <Button text="Visit Project" url={project.link} />
         <Button text="View Code" url={project.github} />
       </div>
     </div>
-  </div>
+  </article>
 )
 
 export const Projects = () => (
-  <section>
+  <section class="h-full">
     <div class="w-full flex flex-col items-center jutify-center">
       <h4>Stuff I've made</h4>
-      <div class="flex">
-        <Project project={projects[0]}/>
-        {/* {projects.map(p => <Project project={p} />)} */}
+      <div class="flex flex-wrap">
+        {projects.map(p => <Project project={p} />)}
       </div>
     </div>
   </section>
