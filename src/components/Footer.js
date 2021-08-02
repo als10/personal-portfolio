3e38a12d39797868cc6f66e14fdfff790b21c35d
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-08-02 16:38:27 +0400

    add socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Socials } from "./Socials"
 
 export const Footer = () => (
-  <footer class="absolute bottom-0 z-50 w-screen bg-gray-800">
-    
+  <footer class="w-screen bg-gray-600 text-white p-8 text-center">
+    <Socials />
+    <div>Created and designed by Alston D'mello</div>
   </footer>
 )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -30,7 +30,7 @@ const MenuButton = ({ menuActive, setMenuActive }) => (
     onClick={() => setMenuActive(!menuActive)}
   >
     {menuActive 
-    ? <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    ? <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
       </svg>
     : <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
