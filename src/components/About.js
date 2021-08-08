4c9384f8b99673545329c1181a29d1648b078926
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-08-09 00:24:37 +0400

    add mdx files

--- src/components/About.js
+++ src/components/About.js
@@ -14,14 +14,7 @@ export const About = () => (
     >
         <Column>
             <h4>Who am I</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin rhoncus finibus quam, condimentum
-                tincidunt erat pharetra ac. Nunc ante sapien, congue vitae felis nec, lobortis viverra augue. Integer
-                vitae nibh vel nunc finibus placerat. Aliquam tincidunt magna eros, nec maximus neque accumsan eget.
-                Etiam vitae convallis massa. Donec diam nulla, volutpat a rutrum nec, pulvinar sit amet neque. Donec
-                eleifend ut magna varius condimentum. Morbi eu erat et dolor porta porttitor eget vitae mauris.
-                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus ornare,
-                risus id ornare sagittis, est augue convallis lectus, et aliquet magna arcu nec arcu. Donec at mollis
-                metus. Aenean et mattis velit. Phasellus id enim magna.</p>
+            <p></p>
         </Column>
         <Column>
             <StaticImage
