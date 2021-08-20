225c8c2bd6b2d877b250ea49ed1e19cbbcdf8877
Alston D'mello <31090569+als10@users.noreply.github.com>
2021-08-20 04:00:45 +0400

    add hover for social links

--- src/components/socials.js
+++ src/components/socials.js
@@ -5,7 +5,7 @@ export default () => (
     <div class="flex space-x-4 justify-center mb-8">
         {socialMediaIds.map(s =>
             <a href={`${s.baseUrl}${s.id}`} target="_blank" rel="noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="currentColor" viewBox="0 0 24 24">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 hover:text-gray-400" fill="currentColor" viewBox="0 0 24 24">
                     <path d={s.path}/>
                 </svg>
             </a>
