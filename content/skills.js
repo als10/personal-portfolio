25f5ffec85064fc89ce54fb9ac4e4878d9fd277a
Alston D'mello <31090569+als10@users.noreply.github.com>
2022-05-08 18:17:56 -0400

    update skills list

---
+++ content/skills.js
@@ -0,0 +1,62 @@
+import AndroidStudio from '../src/images/skills/android-studio.svg'
+import CSharp from '../src/images/skills/c-sharp.svg'
+import C from '../src/images/skills/c.svg'
+import CPP from '../src/images/skills/cpp.svg'
+import CSS from '../src/images/skills/css.svg'
+import Express from '../src/images/skills/express.png'
+import Figma from '../src/images/skills/figma.svg'
+import Firebase from '../src/images/skills/firebase.png'
+import Flutter from '../src/images/skills/flutter.svg'
+import Gatsby from '../src/images/skills/gatsby.png'
+import Git from '../src/images/skills/git.svg'
+import HTML from '../src/images/skills/html.svg'
+import Java from '../src/images/skills/java.svg'
+import JavaScript from '../src/images/skills/javascript.svg'
+import MongoDB from '../src/images/skills/mongodb.svg'
+import MySQL from '../src/images/skills/mysql.svg'
+import NodeJS from '../src/images/skills/nodejs.svg'
+import Python from '../src/images/skills/python.svg'
+import React from '../src/images/skills/react.svg'
+import SQLite from '../src/images/skills/sqlite.png'
+import Tailwind from '../src/images/skills/tailwind.svg'
+import Unity from '../src/images/skills/unity.svg'
+
+export default [
+    {
+        "type": "Frontend",
+        "technologies": [
+            { "name": "ReactJS", "logo": React },
+            { "name": "GatsbyJS", "logo": Gatsby },
+            { "name": "Flutter", "logo": Flutter },
+            { "name": "Tailwind CSS", "logo": Tailwind },
+            { "name": "HTML", "logo": HTML },
+            { "name": "CSS", "logo": CSS },
+            { "name": "JavaScript", "logo": JavaScript }
+        ]
+    },
+    {
+        "type": "Backend",
+        "technologies": [
+            { "name": "NodeJS", "logo": NodeJS },
+            { "name": "Express", "logo": Express },
+            { "name": "MongoDB", "logo": MongoDB },
+            { "name": "MySQL", "logo": MySQL },
+            { "name": "SQLite3", "logo": SQLite },
+            { "name": "Firebase", "logo": Firebase }
+        ]
+    },
+    {
+        "type": "Miscellaneous",
+        "technologies": [
+            { "name": "Java", "logo": Java },
+            { "name": "Python", "logo": Python },
+            { "name": "C", "logo": C },
+            { "name": "C++", "logo": CPP },
+            { "name": "C#", "logo": CSharp },
+            { "name": "Git", "logo": Git },
+            { "name": "Figma", "logo": Figma },
+            { "name": "Unity", "logo": Unity },
+            { "name": "Android Studio", "logo": AndroidStudio }
+        ]
+    }
+]
